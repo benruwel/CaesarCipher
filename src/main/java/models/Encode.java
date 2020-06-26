8af473ade3ca230ca0390df15ef9e1fc2cef91f0
benruwel <benruwel21@gmail.com>
2020-06-26 20:40:16 +0300

    created tests for handling user input

--- src/main/java/models/Encode.java
+++ src/main/java/models/Encode.java
@@ -1,5 +1,28 @@
 package models;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Encode {
+    private String mInputString;
+    private Integer mCipherKey;
+    private char[] mAlphabets = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+    public Encode(String inputString, Integer cipherKey ) {
+        this.mInputString = inputString;
+        this.mCipherKey = cipherKey;
+    }
+
+    public String getInputString() {
+        return mInputString;
+    }
+
+    public Integer getInputCipherKey() {
+        return mCipherKey;
+    }
+
+    public char[] convertInputToCharArray() {
+        return this.mInputString.toCharArray();
+    }
 
 }
--- src/test/java/models/EncodeTest.java
+++ src/test/java/models/EncodeTest.java
@@ -5,6 +5,27 @@ import static org.junit.Assert.*;
 
 public class EncodeTest {
     @Test
-    public void name() {
+    public void newEncode_instantiatesCorrectly() {
+        Encode testEncode = new Encode("Calm down", 2);
+        assertEquals(true, testEncode instanceof Encode);
+    }
+
+    @Test
+    public void newEncode_getsInputString() {
+        Encode testEncode = new Encode("Calm down", 2);
+        assertEquals("Calm down", testEncode.getInputString());
+    }
+    @Test
+    public void newEncode_getsInputCipherKey() {
+        Encode testEncode = new Encode("Calm down", 2);
+        Integer expectedOutput = 2;
+        assertEquals(expectedOutput, testEncode.getInputCipherKey());
+    }
+
+    @Test
+    public void runEncode_storeChar_CharArray() {
+        Encode testEncode = new Encode("Hello world", 2);
+        char [] expectedOutput = testEncode.getInputString().toCharArray();
+        assertArrayEquals(expectedOutput, testEncode.convertInputToCharArray());
     }
 }
